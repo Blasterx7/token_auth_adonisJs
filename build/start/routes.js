4a9b5f60af9d09f9ba0e163f0792ecbfa02f7f36
Georges987 <ayenigeorgepierre@gmail.com>
2025-04-02 12:28:51 +0100

    ci production

---
+++ build/start/routes.js
@@ -0,0 +1,15 @@
+import router from '@adonisjs/core/services/router';
+import { middleware } from './kernel.js';
+import AuthController from '#controllers/auth/auth_controller';
+router.get('/', () => {
+    return 'Hello world from the home page.';
+});
+router.post('/login', [AuthController, 'login']);
+router.post('/register', [AuthController, 'register']);
+router.get('/about', () => {
+    return 'This is the about page.';
+}).use(middleware.auth());
+router.get('/posts/:id', ({ params }) => {
+    return `This is post with id ${params.id}`;
+});
+//# sourceMappingURL=routes.js.map
